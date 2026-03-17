d6788b103656f011f9b6772c7ba4cd431f142f4c
Elena Zen <elenazen563@gmail.com>
2026-03-17 21:11:52 +0100

    Separate frontend and backend

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function show() {
-    const menu = document.getElementById("menucont");
-    const isMobile = window.matchMedia("(max-width: 800px)").matches;
-
-    if (isMobile) {
-        // Se la media query è attiva, mostra/nascondi il menu
-        menu.classList.toggle("openmenu");
-    }
-}
